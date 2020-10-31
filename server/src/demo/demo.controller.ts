befb6de6d11b8aa604274dae4ce466c2e80e9678
Eugeny KOnstantinov <jskonst@yandex.ru>
2020-10-31 13:29:42 +0300

    Add initial apps

---
+++ server/src/demo/demo.controller.ts
@@ -0,0 +1,11 @@
+import { Controller, Get } from '@nestjs/common';
+
+@Controller('demo')
+export class DemoController {
+  constructor() {}
+
+  @Get()
+  getHello(): any {
+    return {"some": 2, "name": "sdfsdf"};
+  }
+}
