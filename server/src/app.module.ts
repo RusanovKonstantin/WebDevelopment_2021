9ffd2eaad74506e4ab0ecf3a3a198d5c308288b8
RusanovKonstantin <kr369501@mail.ru>
2021-09-24 18:11:19 +0300

    test-0.02

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,17 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { DemoController } from './demo/demo.controller';
-// import { GraphQLModule } from '@nestjs/graphql';
-import { CatsModule } from './cats/cats.module';
+
 
 @Module({
-  imports: [
-  //   GraphQLModule.forRoot({
-  //   autoSchemaFile: 'schema.gql',
-  // }),
-CatsModule],
-  controllers: [AppController, DemoController],
+  imports: [],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
