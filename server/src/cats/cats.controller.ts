3db06bcbf4004fb62bbe46637b725f03a305973c
RusanovKonstantin <kr369501@mail.ru>
2021-10-15 09:34:34 +0300

    laba-V0.2

--- server/src/cats/cats.controller.ts
+++ server/src/cats/cats.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, Post, Query } from '@nestjs/common';
 
 @Controller('cats')
 export class CatsController 
 {
     @Get()
-    findAll():string
+    findAll(@Query('a') a: number,@Query('b') b:number):string
     {
-        return "<b>Hellow Cats</b>";
+        const _a = a ? +a:0;
+        const _b=b? +b:0;
+        return `<b>Hellow Cats  a+b=${_a + _b}</b>`;
     }
 
     @Post()
