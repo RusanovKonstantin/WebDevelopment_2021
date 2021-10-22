814507bd2d4f2e9cbe8cf3ba2c0c65d1aeae946b
RusanovKonstantin <kr369501@mail.ru>
2021-10-22 09:15:35 +0300

    laba-V0.4(server_end)

--- server/src/cats/cats.controller.ts
+++ server/src/cats/cats.controller.ts
@@ -1,22 +1,66 @@
-import { Controller, Get, Post, Query, Body } from '@nestjs/common';
+import { Controller, Get, Post, Query, Body, Param, HttpException, HttpStatus, Put, Delete, HttpCode } from '@nestjs/common';
 import { CatDto } from './dto/cat-dto';
 
 @Controller('cats')
-export class CatsController 
-{
+export class CatsController {
+    cats: CatDto[] =
+        [
+            {
+                id: 1,
+                age: 0,
+                name: "Dymok",
+                color: "grow"
+            },
+            {
+                id: 2,
+                age: 2,
+                name: "Murka",
+                color: "three color"
+            }
+        ]
     @Get()
-    findAll(@Query('a') a: number,@Query('b') b:number):string
-    {
-        const _a = a ? +a:0;
-        const _b=b? +b:0;
-        return `<b>Hellow Cats  a+b=${_a + _b}</b>`;
+    findAll(): CatDto[] {
+
+        return this.cats;
+    }
+    @Get(':id')
+    findOne(@Param() params): CatDto {
+        const cat = this.cats.find(cat => cat.id === +params.id);
+
+        if (cat) { return cat }
+        throw new HttpException('Not founded', HttpStatus.NOT_FOUND);
     }
 
     @Post()
-    createCat(@Body() cat: CatDto):CatDto
-    {
-        console.log(cat);
-        console.log(cat.age +1);
+    createCat(@Body() cat: CatDto): CatDto {
+        const id = this.cats.length + 1;
+        cat.id = id;
+        this.cats.push(cat);
         return cat;
+    }
+
+    @Put()
+    UpdateOne(@Body() cat: CatDto): CatDto {
+        const foundCat = this.cats.find(item => item.id === cat.id);
+
+        if (foundCat) 
+        { 
+            this.cats[foundCat.id - 1] = cat;
+             return this.cats[foundCat.id - 1] 
+        }
+        throw new HttpException('Not founded', HttpStatus.NOT_FOUND);
+    }
+
+    @Delete(':id')
+    @HttpCode(202)
+    delete(@Param() parms)
+    {
+        const lengthBefor = this.cats.length;
+        this.cats = this.cats.filter(item => item.id != +parms.id);
+        if (lengthBefor === this.cats.length)
+        {
+            throw new HttpException('Not found', HttpStatus.NOT_FOUND)
+        }
+
     }
 }
