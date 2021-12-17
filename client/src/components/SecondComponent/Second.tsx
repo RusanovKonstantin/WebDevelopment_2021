00879206732d6e0be627f9c41fd5a00fb1fb2c20
RusanovKonstantin <kr369501@mail.ru>
2021-12-17 09:40:22 +0300

    Client_Output_Text

--- client/src/components/SecondComponent/Second.tsx
+++ client/src/components/SecondComponent/Second.tsx
@@ -1,17 +1,43 @@
-import { Button } from '@material-ui/core'
+import { Button, Grid, TextField } from '@material-ui/core'
 import React, {useState} from 'react'
 
 export default function Second() {
     const text = "variable"
   const [counter, setCounter] = useState<number>(0);
+  const [caption, setMyText] = useState<string>("");
+  const [display, setDisplay] = useState<string>("");
 
     const clickHandler = () =>{
         setCounter(counter + 1)
         console.log(`Hello ${counter}`);
     }
+    const onTextChange = (event: any) => {
+        setMyText(event.target.value);
+    }
+    const textClickHandler = () =>{
+        setDisplay(caption)
+    }
+
     return (
         <div>
-            <h1>Cats{text}</h1>
+        <h1>Hello {text}</h1>
+        <Grid container spacing={2}>
+          <Grid item xs={1}>
+            <TextField
+              id="standard-basic"
+              label="Standard"
+              variant="standard"
+              onChange={onTextChange}
+            />
+          </Grid>
+          <Grid item xs={1}>
+            <Button variant="contained" onClick={textClickHandler}>
+              Send info
+            </Button>
+          </Grid>
+        </Grid>
+
+            <h3>You entered: {display} </h3>
             <h2> You clicked {counter}</h2>
             <Button variant="contained" onClick={clickHandler}>Contained</Button>
         </div>
